ed0d31c8d913ac4c7f8cf496bd04af596890f780
Sreyas Pillai <sreyasmpillai24@gmail.com>
2020-09-01 17:51:05 +0530

    Server push

--- server/index.js
+++ server/index.js
@@ -3,6 +3,27 @@ var path = require('path');
 
 var app = express();
 
+var fileupload = require('express-fileupload');
+app.use(fileupload());
+
+app.post("/upload",function(req,res,next){
+    // console.log(req.files)
+    const file = req.files.photo;
+    file.mv("./uploads/" + file.name, function(err, result){
+        if(err)
+            throw err;
+    
+        res.send({
+             success: true,
+             message: "image uploaded!"
+    });
+ });
+
+})
+
+
+
+
 
 var port = 3000;
 app.listen(port, function() {
