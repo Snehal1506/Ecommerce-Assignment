dc981289c4e54870d2a04405dfa952e333f6e475
Snehal1506 <snehallokhande156@gmail.com>
2020-09-02 17:28:51 +0530

    connection changes

--- server/index.js
+++ server/index.js
@@ -96,7 +96,7 @@ app.use('/admin/products',adminProducts);
 
 
 //Start the server
-var port = 5000;
+var port = 3000;
 app.listen(port, function() {
     console.log('Server started on port '+ port)
 });
